37f3cdae0b268c42de9bfcacebba7376c489543d
Prabhav Bhatnagar <prabhav2b@gmail.com>
2021-03-09 16:19:55 +0200

    basic setup

---
+++ Assets/Scripts/ColorPicker.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System;
+
+public class ColorPicker : MonoBehaviour
+{
+
+    private Color color;
+
+    public void SetRed() => color = Color.red;
+    public void SetBlue() => color = Color.blue;
+    public void SetYellow() => color = Color.yellow;
+}
---
+++ Assets/Scripts/MoveForward.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveForward : MonoBehaviour
+{
+
+    [SerializeField] private float speed = 5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float posUpdate = Time.deltaTime;
+
+        this.transform.position += Vector3.right * posUpdate * 5f;
+    }
+}
